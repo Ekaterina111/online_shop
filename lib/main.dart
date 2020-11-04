bfc54b18d649d7fd63e5f533118b3dfe10dd503e
Ekaterina111 <ekaterinaegorova70913@gmail.com>
2020-11-04 21:18:08 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:online_shop/screens/products.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Drawer and Navigation',
       theme: ThemeData(
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  final List<Page> _pages = [
+    Page('Список товаров', Icons.home),
+    Page('Корзина', Icons.add_shopping_cart),
+    Page('Профиль', Icons.person_outline),
+  ];
+
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentPageIndex = 0;
 
+  void _openPage(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
+       return Scaffold(
       appBar: AppBar(
-        title: Text("MyApp"),
+        title: Text("МАГАЗИН"),
       ),
-      body: ProductsScreen(),
-    );
+      body: 
+ 
+             ProductsScreen(),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        items: widget._pages.map((Page page) =>
+            BottomNavigationBarItem(
+              icon: Icon(page.iconData),
+              title: Text(page.title),
+            )).toList(),
+        onTap: _openPage,
+      ),
+      );
   }
+}
+
+class Page {
+  final String title;
+  final IconData iconData;
+  Page(this.title, this.iconData);
 }
